3a102ddb27ded19594f014f85dabea5827c6f220
Florian Latzel <florian.latzel@is-loesungen.de>
2011-12-12 00:20:36 +0100

    Added "$alias[dev]" from aliases.drushrc.php.

--- doxygen/example.aliases.drushrc.php
+++ doxygen/example.aliases.drushrc.php
@@ -4,8 +4,30 @@
  * Additional explaination of nagios cammand specific options
  */
 
+
 /**
- * @ingroup nagios_command_specific
+ * @defgroup nagios_command_specific aliases.drush.inc
+ * Nagios command specific options for drush site aliases
+ * @{
+ */
+
+/**
+ * Example site alias "dev" with command specific options for nagios, usual aliases.drushrc.php notation
+ */
+$aliases['dev'] = array(
+  'uri' => 'dev.mydrupalsite.com',
+  'root' => '/path/to/drupal/root',
+
+  'command-specific' => array (
+    'nagios' => array(
+      'ignore' => 'project1,project2,projectN', 
+      'show-versions' => TRUE,
+      'changelog' => TRUE, 
+    ),
+  )
+);
+
+/**
  * Comma seperated list of projects which should be ignored, useful for patched, modified modules etc 
  *
  * @see $aliases['dev'] for usual notation 
@@ -13,7 +35,6 @@
 $aliases['dev']['command-specific']['nagios']['ignore'] = 'project1,project2,projectN';
 
 /**
- * @ingroup nagios_command_specific
  * Show used / current and recommended versions for a affected project  
  *
  * @see $aliases['dev'] for usual notation 
@@ -21,10 +42,10 @@ $aliases['dev']['command-specific']['nagios']['ignore'] = 'project1,project2,pro
 $aliases['dev']['command-specific']['nagios']['show-versions'] = TRUE;
 
 /**
- * @ingroup nagios_command_specific
  * Show release link / link to changelog for the recommended version of a affected projects
  *
  * @see $aliases['dev'] for usual notation 
  */
 $aliases['dev']['command-specific']['nagios']['changelog'] = TRUE;;
 
+/** @} */ /** End of nagios_command_specific */
